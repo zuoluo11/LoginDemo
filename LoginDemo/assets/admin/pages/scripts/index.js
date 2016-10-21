9d16c6e2aec483e9bab3e7c7974b81156c614247
zuoluo11 <zuoluo11@qq.com>
2016-10-21 17:40:19 +0800

    首页菜单练习

--- LoginDemo/assets/admin/pages/scripts/index.js
+++ LoginDemo/assets/admin/pages/scripts/index.js
@@ -155,7 +155,7 @@ var Index = function () {
                     backgroundColor: Metronic.getBrandColor('green')
                 }, {
                     title: 'Meeting',
-                    start: new Date(y, m, d+9, 10, 30),
+                    start: new Date(y, m, d + 9, 10, 30),
                     allDay: false
                 }, {
                     title: 'Lunch',
@@ -435,11 +435,11 @@ var Index = function () {
 
                     var aArgs = Array.prototype.slice.call(arguments, 1),
                         fToBind = this,
-                        fNOP = function () {},
+                        fNOP = function () { },
                         fBound = function () {
                             return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
                         aArgs.concat(Array.prototype.slice.call(arguments)));
-                    };
+                        };
 
                     fNOP.prototype = this.prototype;
                     fBound.prototype = new fNOP();
@@ -577,42 +577,42 @@ var Index = function () {
             }
 
             $('#dashboard-report-range').daterangepicker({
-                    opens: (Metronic.isRTL() ? 'right' : 'left'),
-                    startDate: moment().subtract('days', 29),
-                    endDate: moment(),
-                    minDate: '01/01/2012',
-                    maxDate: '12/31/2014',
-                    dateLimit: {
-                        days: 60
-                    },
-                    showDropdowns: false,
-                    showWeekNumbers: true,
-                    timePicker: false,
-                    timePickerIncrement: 1,
-                    timePicker12Hour: true,
-                    ranges: {
-                        'Today': [moment(), moment()],
-                        'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                        'Last 7 Days': [moment().subtract('days', 6), moment()],
-                        'Last 30 Days': [moment().subtract('days', 29), moment()],
-                        'This Month': [moment().startOf('month'), moment().endOf('month')],
-                        'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-                    },
-                    buttonClasses: ['btn btn-sm'],
-                    applyClass: ' blue',
-                    cancelClass: 'default',
-                    format: 'MM/DD/YYYY',
-                    separator: ' to ',
-                    locale: {
-                        applyLabel: 'Apply',
-                        fromLabel: 'From',
-                        toLabel: 'To',
-                        customRangeLabel: 'Custom Range',
-                        daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                        monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                        firstDay: 1
-                    }
+                opens: (Metronic.isRTL() ? 'right' : 'left'),
+                startDate: moment().subtract('days', 29),
+                endDate: moment(),
+                minDate: '01/01/2012',
+                maxDate: '12/31/2014',
+                dateLimit: {
+                    days: 60
                 },
+                showDropdowns: false,
+                showWeekNumbers: true,
+                timePicker: false,
+                timePickerIncrement: 1,
+                timePicker12Hour: true,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+                    'Last 7 Days': [moment().subtract('days', 6), moment()],
+                    'Last 30 Days': [moment().subtract('days', 29), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+                },
+                buttonClasses: ['btn btn-sm'],
+                applyClass: ' blue',
+                cancelClass: 'default',
+                format: 'MM/DD/YYYY',
+                separator: ' to ',
+                locale: {
+                    applyLabel: 'Apply',
+                    fromLabel: 'From',
+                    toLabel: 'To',
+                    customRangeLabel: 'Custom Range',
+                    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+                    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                    firstDay: 1
+                }
+            },
                 function (start, end) {
                     $('#dashboard-report-range span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
                 }
@@ -621,6 +621,36 @@ var Index = function () {
 
             $('#dashboard-report-range span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
             $('#dashboard-report-range').show();
+        },
+        //≥ı ºªØ≤Àµ•
+        initMenu: function () {
+            //$.ajax({
+            //    url: "Login/SendEmail",
+            //    type: "post",
+            //    data: $(".forget-form").serialize(),
+            //    success: function (result) {
+            //        alert(result);
+            //        //window.location = "Login/Index";
+            //    },
+            //    error: function (result) {
+            //        alert(result);
+            //    }
+            //})
+            var menu = $(' <li>'
+                       + '  <a href="javascript:;">'
+                       + '      <i class="icon-puzzle"></i>'
+                        + '     <span class="title">UI Components</span>'
+                        + '     <span class="arrow  open"></span>'
+                        + ' </a>'
+                        + ' <ul class="sub-menu" style="display: block;">'
+                        + '     <li>'
+                        + '        <a href="components_pickers.html">'
+                        + '           Pickers'
+                        + '        </a>'
+                        + '    </li>'
+                        + '</ul>'
+                    + ' </li>')
+           $('.sidebar-search-wrapper').after(menu);
         }
 
     };
