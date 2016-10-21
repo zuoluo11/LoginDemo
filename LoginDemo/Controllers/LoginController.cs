7e4c2f987e1449b9562f82806a0dbee1ca3dfe74
zuoluo11 <zuoluo11@qq.com>
2016-10-21 15:57:30 +0800

    提交登录找回密码

--- LoginDemo/Controllers/LoginController.cs
+++ LoginDemo/Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using CYQ.Data.Table;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using TY.Enum.LoginDemo;
@@ -70,6 +72,35 @@ namespace LoginDemo.Controllers
             return RedirectToAction("Index", "Login");
         }
 
+        public ActionResult Repassword(string code)
+        {
+            ViewBag.code = code;
+            ViewBag.userName = "Amanda Smith";
+            return View();
+        }
+        /// <summary>
+        /// 重设密码
+        /// </summary>
+        /// <returns></returns>
+        public string setPassword()
+        {
+            string id = "63de5222-6903-4a2a-9aa1-38dc79468f03";
+            string result = "0";
+            AppConfig.Debug.OpenDebugInfo = true;
+            AppDebug.Start();
+            using (MAction action = new MAction(TableNames.Base_User))
+            {
+                if (action.Fill(id))
+                {
+                    action.Update();
+                    result = "1";
+                }
+               
+            }
+            //Console.WriteLine(AppDebug.Info);
+            AppDebug.Stop();
+            return result;
+        }
 
         /// <summary>
         /// 发送找回密码邮件
@@ -77,97 +108,81 @@ namespace LoginDemo.Controllers
         /// <returns></returns>
         public string SendEmail(string email)
         {
-            int IsOk = 0;
-            string Message = string.Empty;
+            string message = string.Empty;
 
-
-            //检查邮箱
-            if (string.IsNullOrEmpty(email))
+            using (MAction action = new MAction(TableNames.Base_User))
             {
-                Message = "对不起，邮箱不可为空！";
-                return Content(new JsonMessage { Success = false, Code = IsOk.ToString(), Message = Message }.ToString());
+                action.SetPara("Email", email);
+                if (action.Fill("Email=@Email"))
+                {
+                    ////生成随机码
+                    string strcode = Guid.NewGuid().ToString();
+                    string titletxt = "密码找回";
+                    string bodytxt = "这是主体" + "http://" + HttpContext.Request.Url.Authority.ToLower()
+                    + "/Login/Repassword?code=" + strcode;
+                    try
+                    {
+                        SendEmail("309957373@qq.com", titletxt, bodytxt, true);
+                        message = "发送成功";
+                    }
+                    catch (Exception)
+                    {
+                        message = "发送异常";
+
+                    }
+                }
+                else
+                {
+                    message = "对不起，您输入的邮箱用户不存在！";
+                }
             }
-
-
-            //检查用户信息
-            Base_UserBll bll = new Base_UserBll();
-            Base_User model = bll.Repository().FindEntity("Email", email);
-            if (string.IsNullOrEmpty(model.UserId))
-            {
-                Message = "对不起，您输入的邮箱用户不存在！";
-                return Content(new JsonMessage { Success = false, Code = IsOk.ToString(), Message = Message }.ToString());
-            }
-            else
-            {
-                //发送取回密码的短信或邮件
-                #region 发送邮件==================
-
-                ////生成随机码
-                string strcode = Guid.NewGuid().ToString();
-                ////获得邮件内容
-
-                Base_MailTemplate mailModel = new Base_MailTemplateBll().Repository().FindEntity("CallIndex", "getpassword");
-                if (string.IsNullOrEmpty(mailModel.MailTemplateGuid))
-                {
-                    Message = "邮件发送失败，邮件模板内容不存在！";
-                    return Content(new JsonMessage { Success = false, Code = IsOk.ToString(), Message = Message }.ToString());
-                }
-                //检查是否重复提交
-
-                Base_UserCodeBll codeBll = new Base_UserCodeBll();
-                Base_UserCode codeModel;
-                codeModel = codeBll.Repository().FindEntityBySql(string.Format(("select * from Base_UserCode where status=0 and datediff({0},EffTime,getdate())<=0 and UserId='{1}'"), "d", model.UserId));
-                if (string.IsNullOrEmpty(codeModel.UserCodeGuid))
-                {
-                    codeModel = new Base_UserCode();
-                    codeModel.Create();
-                    //写入数据库
-                    codeModel.UserId = model.UserId;
-                    codeModel.UserName = model.RealName;
-                    codeModel.Type = "FindPWD";
-                    codeModel.StrCode = strcode;
-                    codeModel.EffTime = DateTime.Now.AddDays(1);
-                    codeModel.AddTime = DateTime.Now;
-                    codeModel.Count = 0;
-                    codeModel.Status = 0;
-                    codeModel.UserIP = NetHelper.GetIPAddress();
-                    codeBll.Repository().Insert(codeModel);
-                }
-                //替换模板内容
-                string titletxt = mailModel.MailTitle;
-                string bodytxt = mailModel.MailContent;
-                titletxt = titletxt.Replace("{webname}", ConfigHelper.AppSettings("CustomerCompanyName"));
-                titletxt = titletxt.Replace("{username}", model.RealName);
-                bodytxt = bodytxt.Replace("{webname}", ConfigHelper.AppSettings("SoftName"));
-                bodytxt = bodytxt.Replace("{valid}", "1");//有效期
-                bodytxt = bodytxt.Replace("{username}", model.RealName);
-                bodytxt = bodytxt.Replace("{webtel}", ConfigHelper.AppSettings("Contact"));
-                bodytxt = bodytxt.Replace("{weburl}", "http://" + HttpContext.Request.Url.Authority.ToLower());
-
-                bodytxt = bodytxt.Replace("{linkurl}", "http://" + HttpContext.Request.Url.Authority.ToLower()
-                    + "/Login/Repassword?code=" + codeModel.StrCode);
-
-                //发送邮件
-                try
-                {
-                    NetHelper.SendEmail(model.Email, titletxt, bodytxt, true);
-                }
-                catch
-                {
-                    Message = "邮件发送失败，请联系本站管理员！";
-                    return Content(new JsonMessage { Success = false, Code = IsOk.ToString(), Message = Message }.ToString());
-
-                }
-                Message = "邮件发送成功，请登录邮箱查看邮件！";
-                IsOk = 1;
-                return Content(new JsonMessage { Success = true, Code = IsOk.ToString(), Message = Message }.ToString());
-
-                #endregion
-            }
-
-
+            return message;
 
         }
+
+        #region 发送电子邮件
+        /// <summary>
+        /// 发送电子邮件,所有SMTP配置信息均在config配置文件中system.net节设置.
+        /// </summary>
+        /// <param name="receiveEmail">接收电子邮件的地址</param>
+        /// <param name="msgSubject">电子邮件的标题</param>
+        /// <param name="msgBody">电子邮件的正文</param>
+        /// <param name="IsEnableSSL">是否开启SSL</param>
+        public static bool SendEmail(string receiveEmail, string msgSubject, string msgBody, bool IsEnableSSL)
+        {
+            //创建电子邮件对象
+            MailMessage email = new MailMessage();
+            //设置接收人的电子邮件地址
+            email.To.Add(receiveEmail);
+            //设置邮件的标题
+            email.Subject = msgSubject;
+            //设置邮件的正文
+            email.Body = msgBody;
+            //设置邮件为HTML格式
+            email.IsBodyHtml = true;
+            email.From = new MailAddress("zuoluo11@qq.com");
+
+            //创建SMTP客户端，将自动从配置文件中获取SMTP服务器信息
+            SmtpClient smtp = new SmtpClient("smtp.qq.com");
+            smtp.Credentials = new NetworkCredential("zuoluo11@qq.com", "dxtgkuzsirytbjbd");
+            //开启SSL
+            smtp.EnableSsl = IsEnableSSL;
+
+
+            try
+            {
+                //发送电子邮件
+                smtp.Send(email);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
     }
 
 
--- LoginDemo/assets/admin/pages/scripts/login.js
+++ LoginDemo/assets/admin/pages/scripts/login.js
@@ -1,7 +1,7 @@
 var Login = function () {
 
     var handleLogin = function () {
-
+       
         $('.login-form').validate({
             errorElement: 'span', //default input error message container
             errorClass: 'help-block', // default input error message class
@@ -47,27 +47,65 @@ var Login = function () {
 
             submitHandler: function (form) {
                 //form.submit(); // form validation success, call ajax form submit
-                var userName = $("input[name='username']").val();
-                var passWord = $("input[name='password']").val();
-                $.post("/Login/Login", { userName: userName, passWord: passWord }, function (data) {
-                    if (data == "0") {
-                        alert("出错");
-                    } else {
-                        window.location.href = "/Home/Index";
-                    }
-
-                });
+                login();
             }
         });
+
+        var login = function () {
+            var userName = $("input[name='username']").val();
+            var passWord = $("input[name='password']").val();
+            $.post("/Login/Login", { userName: userName, passWord: passWord }, function (data) {
+                if (data == "0") {
+                    alert("出错");
+                } else {
+                    //记住或删除密码
+                    setCookie(userName,passWord);
+                    window.location.href = "/Home/Index";
+                }
+
+            });
+        }
+
+        //记住密码
+        var setCookie = function (loginCode, pwd) { //设置cookie
+            if ($("input[name='remember']").parent().hasClass("checked")) { //判断是否选中了“记住密码”复选框
+                $.cookie("txtaccount", loginCode, { expires: 7 });//调用jquery.cookie.js中的方法设置cookie中的用户名
+                //$.cookie("pwd", base64Encode(pwd), { expires: 7 });//调用jquery.cookie.js中的方法设置cookie中的登陆密码，并使用base64进行加密
+                $.cookie("pwd", pwd, { expires: 7 });
+               
+            } else {
+                $.cookie("pwd", pwd, { expires: -1});
+               
+            }
+        }
+        var getCookie = function () { //获取cookie
+            var loginCode = $.cookie("txtaccount"); //获取cookie中的用户名
+            var pwd = $.cookie("pwd"); //获取cookie中的登陆密码
+            if (pwd) {//密码存在的话把“记住用户名和密码”复选框勾选住
+                //$("input[name='remember']").attr("checked", "true");
+                $("input[name='remember']").parent().addClass("checked");
+                
+            }
+            if (loginCode) {//用户名存在的话把用户名填充到用户名文本框
+                $("input[name='username']").val(loginCode);
+            }
+            if (pwd) {//密码存在的话把密码填充到密码文本框
+                //不安全
+                //$("#txtpassword").val(base64Decode(pwd));
+                $("input[name='password']").val(pwd);
+            }
+        }
 
         $('.login-form input').keypress(function (e) {
             if (e.which == 13) {
                 if ($('.login-form').validate().form()) {
-                    //$('.login-form').submit(); //form validation success, call ajax form submit
+                   //form validation success, call ajax form submit
+                    login();
                 }
                 return false;
             }
         });
+        getCookie();//记住密码
     }
 
     var handleForgetPassword = function () {
@@ -112,7 +150,7 @@ var Login = function () {
                 $('.forget-form .btn-success').trigger(onclick);
             }
         });
-        //$(".forget-form").attr("action", "Login/SendEmail").attr("method", "post");
+      
 
         $('.forget-form .btn-success').click(function () {
             if ($(".forget-form").valid()) {
@@ -122,7 +160,7 @@ var Login = function () {
                     data: $(".forget-form").serialize(),
                     success: function (result) {
                         alert(result);
-                        window.location = "Home/Index";
+                        //window.location = "Login/Index";
                     },
                     error: function (result) {
                         alert(result);
