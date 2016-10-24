f366667efadc826b1d54d347c3b356979319ca8a
zuoluo11 <zuoluo11@qq.com>
2016-10-24 17:46:18 +0800

    修改增加模板页

---
+++ LoginDemo/Controllers/EasyRememberController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginDemo.Controllers
+{
+    public class EasyRememberController : Controller
+    {
+        //
+        // GET: /EasyRemember/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
